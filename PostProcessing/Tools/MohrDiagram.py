ca3a025d8b340e8c5f5f5f44f46af1e4a09c38b3
Arthur Bauville <a.bauville@gmail.com>
2017-07-04 19:07:38 +0900

    mohr diagram plot tools

---
+++ PostProcessing/Tools/MohrDiagram.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 17:12:02 2017
+
+@author: abauville
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+sxx = -1.0
+sxy = .5
+syx = sxy
+syy = -sxx
+
+
