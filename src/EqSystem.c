abauville/Sunaba

commit 4bcc44e2c4673994bf649545335db0e3b359d4ee

Warning: Modification to the stress boundary conditions Fixed (I think) the implementation for NeumannGhost + added Neumann Doesn't change anything for 0 flux conditions (Free slip or zero flux for temperature), because the right hand side value is always zero with the bug or with the fix. Hopefully, now a non zero value for shear stress or normal stress can be specified (also a given temperature gradient)

src/EqSystem.c
@@ -159,6 +159,45 @@ void EqSystem_assemble(EqSystem* EqSystem, Grid* Grid, BC* BC, Physics* Physics,
 					EqSystem->b[iEq] += -Vloc[order[i]] * 2.0*BC->value[IBC];
 
 				}
+				else if (BC->type[IBC]==Neumann) { // NeumannGhost
+					Vloc[order[Ic]] += Vloc[order[i]]; // +1 to VxC
+
+
+
+					switch (Stencil) {
+					case Stencil_Stokes_Darcy_Momentum_x:
+					case Stencil_Stokes_Momentum_x:
+						if 		(i==1) { // VxW
+							//EqSystem->b[iEq] += -Vloc[i] * BC->value[IBC] * dy;
+							EqSystem->b[iEq] += + BC->value[IBC]/Grid->dx;//-Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
+						}
+						else if (i==3) { // VxE
+							//EqSystem->b[iEq] += +Vloc[i] * BC->value[IBC] * dy;
+							EqSystem->b[iEq] += - BC->value[IBC]/Grid->dx;//+Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyS-1];
+						}
+						break;
+
+					case Stencil_Stokes_Darcy_Momentum_y:
+					case Stencil_Stokes_Momentum_y:
+						if 		(i==4) { // VyS
+							//EqSystem->b[iEq] += -Vloc[i] * BC->value[IBC] * dx;
+							EqSystem->b[iEq] += + BC->value[IBC]/Grid->dy;//-Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
+						}
+						else if (i==8) { // VyN
+							//EqSystem->b[iEq] += +Vloc[i] * BC->value[IBC] * dx;
+							EqSystem->b[iEq] += - BC->value[IBC]/Grid->dy;//+Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxS-1];
+						}
+						break;
+
+					case Stencil_Stokes_Continuity:
+					case Stencil_Stokes_Darcy_Continuity:
+					case Stencil_Stokes_Darcy_Darcy:
+					case Stencil_Poisson:
+					case Stencil_Heat:
+						break;
+					}
+
+				}
 				else if (BC->type[IBC]==NeumannGhost) { // NeumannGhost
 					Vloc[order[Ic]] += Vloc[order[i]]; // +1 to VxC
 
@@ -169,11 +208,11 @@ void EqSystem_assemble(EqSystem* EqSystem, Grid* Grid, BC* BC, Physics* Physics,
 					case Stencil_Stokes_Momentum_x:
 						if 		(i==0) { // VxS
 							//EqSystem->b[iEq] += -Vloc[i] * BC->value[IBC] * dy;
-							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
+							EqSystem->b[iEq] += + BC->value[IBC]/Grid->DYEC[0];//-Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
 						}
 						else if (i==4) { // VxN
 							//EqSystem->b[iEq] += +Vloc[i] * BC->value[IBC] * dy;
-							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyS-1];
+							EqSystem->b[iEq] += - BC->value[IBC]/Grid->DYEC[Grid->nyS-1];//+Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyS-1];
 						}
 						break;
 
@@ -181,11 +220,11 @@ void EqSystem_assemble(EqSystem* EqSystem, Grid* Grid, BC* BC, Physics* Physics,
 					case Stencil_Stokes_Momentum_y:
 						if 		(i==5) { // VyW
 							//EqSystem->b[iEq] += -Vloc[i] * BC->value[IBC] * dx;
-							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
+							EqSystem->b[iEq] += + BC->value[IBC]/Grid->DXEC[0];//-Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
 						}
 						else if (i==7) { // VyE
 							//EqSystem->b[iEq] += +Vloc[i] * BC->value[IBC] * dx;
-							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxS-1];
+							EqSystem->b[iEq] += - BC->value[IBC]/Grid->DXEC[Grid->nxS-1];//+Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxS-1];
 						}
 						break;
 
@@ -200,13 +239,13 @@ void EqSystem_assemble(EqSystem* EqSystem, Grid* Grid, BC* BC, Physics* Physics,
 					case Stencil_Heat:
 
 						if (i==0) { // S
-							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
+							EqSystem->b[iEq] += +BC->value[IBC] / Grid->DYEC[0];
 						} else if (i==1) { // W
-							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
+							EqSystem->b[iEq] += +BC->value[IBC] / Grid->DXEC[0];
 						} else if (i==3) { // E
-							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxEC-2];
+							EqSystem->b[iEq] += -BC->value[IBC] / Grid->DXEC[Grid->nxEC-2];
 						} else if (i==4) { // N
-							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyEC-2];
+							EqSystem->b[iEq] += -BC->value[IBC] / Grid->DYEC[Grid->nyEC-2];
 						}
 
 						// For the moment only 0 gradient is implement

Setups/Benchmarks/KarmanVortex.py
@@ -172,10 +172,10 @@ Physics.gx = 0.0
 
 ##                 BC
 ## =====================================
-#BCStokes.SetupType = "CornerFlow"
+BCStokes.SetupType = "WindTunnel"
 
 
-#BCStokes.refValue       = VatBound
+BCStokes.refValue       = VatBound
 
 #BCThermal.TB = 1300.0 + 273.0
 #BCThermal.TT = 0.0    + 273.0

Setups/Plastic_ShearBands/Corner.py
@@ -302,7 +302,7 @@ Visu.writeImages = True
 Visu.outputFolder = "/Users/abauville/GoogleDrive/Output_Corner/G%.1e_phi%.1e/" % (Sediment.G, Sediment.phiIni)
 Visu.transparency = True
 
-#Visu.showGlyphs = True
+Visu.showGlyphs = True
 Visu.glyphMeshType = "Triangle"
 Visu.glyphScale = 0.5/(BCStokes.refValue/(Char.length/Char.time))
 glyphSpacing = (Grid.ymax-Grid.ymin)/8 #50 * km

Setups/Plastic_ShearBands/Sandbox.py
@@ -155,8 +155,8 @@ if ProductionMode:
     Grid.nxC = round(1/1*((64+64+128)*LWRatio)) #round( RefinementFac*(Grid.ymax-Grid.ymin)/ CompactionLength)
     Grid.nyC = round(1/1*((64+64+128)))#round( RefinementFac*(Grid.xmax-Grid.xmin)/ CompactionLength)
 else:
-    Grid.nxC = round(1/1*((64+32)*LWRatio)) #round( RefinementFac*(Grid.ymax-Grid.ymin)/ CompactionLength)
-    Grid.nyC = round(1/1*((64+32)))#round( RefinementFac*(Grid.xmax-Grid.xmin)/ CompactionLength)
+    Grid.nxC = round(2/1*((64+32)*LWRatio)) #round( RefinementFac*(Grid.ymax-Grid.ymin)/ CompactionLength)
+    Grid.nyC = round(2/1*((64+32)))#round( RefinementFac*(Grid.xmax-Grid.xmin)/ CompactionLength)
 
 Grid.fixedBox = True
 
@@ -274,9 +274,9 @@ Numerics.dtAlphaCorr = .3
 Numerics.absoluteTolerance = 1e-6
 
 
-Numerics.dtMaxwellFac_EP_ov_E  = .9;   # lowest,       ElastoPlasticVisc   /   G
+Numerics.dtMaxwellFac_EP_ov_E  = .5;   # lowest,       ElastoPlasticVisc   /   G
 Numerics.dtMaxwellFac_VP_ov_E  = .0;   # intermediate, ViscoPlasticVisc    /   G
-Numerics.dtMaxwellFac_VP_ov_EP = .0;   # highest,      ViscoPlasticVisc    /   ElastoPlasticStress
+Numerics.dtMaxwellFac_VP_ov_EP = .5;   # highest,      ViscoPlasticVisc    /   ElastoPlasticStress
 Numerics.use_dtMaxwellLimit = True
 
 Numerics.maxTime = (Grid.xmax-Grid.xmin)/abs(VatBound)
@@ -381,7 +381,7 @@ Visu.particleMeshRes = 6
 Visu.particleMeshSize = 1.5*(Grid.xmax-Grid.xmin)/Grid.nxC
 
 Visu.shaderFolder = "../Shaders/Sandbox_w_Layers" # Relative path from the running folder (of StokesFD)
-
+#Visu.shaderFolder = "../Shaders/CornerFlow"
 
 Visu.type = "StrainRate"
 #if ProductionMode:
@@ -396,9 +396,8 @@ Visu.outputFolder = "/Users/abauville/StokesFD_Outputs/Seismic_Sandbox_Outputs/n
 Visu.transparency = False
 
 #Visu.showGlyphs = True
-Visu.glyphType = "DarcyGradient"
 Visu.glyphMeshType = "Triangle"
-Visu.glyphScale = 0.0001/(BCStokes.refValue/(Char.length/Char.time))
+Visu.glyphScale = 8.0/(abs(VatBound)/(Char.length/Char.time))
 glyphSpacing = (Grid.ymax-Grid.ymin)/8 #50 * km
 Visu.glyphSamplingRateX = round(Grid.nxC/((Grid.xmax-Grid.xmin)/glyphSpacing))
 Visu.glyphSamplingRateY = round(Grid.nyC/((Grid.ymax-Grid.ymin)/glyphSpacing))

src/Physics.c
@@ -2704,7 +2704,7 @@ void Physics_computeStrainRateInvariantForOneCell(Physics* Physics, Grid* Grid, 
 	dVydy = (Physics->Vy[(ix) + (iy)*Grid->nxVy]
 						 - Physics->Vy[(ix) + (iy-1)*Grid->nxVy])/Grid->dy;
 
-	// Method A: using the averageing of derivatives on the four nodes
+	// Method A: using the averaging of derivatives on the four nodes
 	// Compute Eps_xy at the four nodes of the cell
 	// 1. Sum contributions
 	dVxdy = 0;

src/stokes.h
@@ -57,7 +57,7 @@
 
 
 
-#define INERTIA true
+#define INERTIA false
 
 
 
@@ -703,7 +703,7 @@ struct Visu
 
 // Boundary conditions
 // ========================
-typedef enum {Dirichlet, DirichletGhost, NeumannGhost, Infinity} BCType;
+typedef enum {Dirichlet, DirichletGhost, Neumann, NeumannGhost, Infinity} BCType;
 typedef enum {Stokes_PureShear, Stokes_SimpleShear, Stokes_FixedLeftWall, Stokes_Sandbox, Stokes_SandboxWeakBackstop, Stokes_CornerFlow,
 			  Thermal_TT_TB_LRNoFlux, Thermal_TT_TBExternal_LRNoFlux,
 			  Darcy_Default} SetupType;

commit c41a944dbd67a08c53daf754865fe687871af48a

Fixed a bug on NeumannGhost right-hand side contribution introduced in commit 4bcc44e of July 6 Now it's back to the way it was before seems proper

src/EqSystem.c
@@ -244,11 +244,13 @@ void EqSystem_assemble(EqSystem* EqSystem, Grid* Grid, BC* BC, Physics* Physics,
 					case Stencil_Stokes_Momentum_x:
 						if 		(i==0) { // VxS
 							//EqSystem->b[iEq] += -Vloc[i] * BC->value[IBC] * dy;
-							EqSystem->b[iEq] += + BC->value[IBC]/Grid->DYEC[0];//-Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
+							//EqSystem->b[iEq] += + BC->value[IBC]/Grid->DYEC[0];//-Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
+							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
 						}
 						else if (i==4) { // VxN
 							//EqSystem->b[iEq] += +Vloc[i] * BC->value[IBC] * dy;
-							EqSystem->b[iEq] += - BC->value[IBC]/Grid->DYEC[Grid->nyS-1];//+Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyS-1];
+							//EqSystem->b[iEq] += - BC->value[IBC]/Grid->DYEC[Grid->nyS-1];//+Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyS-1];
+							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyS-1];
 						}
 						break;
 
@@ -256,11 +258,13 @@ void EqSystem_assemble(EqSystem* EqSystem, Grid* Grid, BC* BC, Physics* Physics,
 					case Stencil_Stokes_Momentum_y:
 						if 		(i==5) { // VyW
 							//EqSystem->b[iEq] += -Vloc[i] * BC->value[IBC] * dx;
-							EqSystem->b[iEq] += + BC->value[IBC]/Grid->DXEC[0];//-Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
+							//EqSystem->b[iEq] += + BC->value[IBC]/Grid->DXEC[0];//-Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
+							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
 						}
 						else if (i==7) { // VyE
 							//EqSystem->b[iEq] += +Vloc[i] * BC->value[IBC] * dx;
-							EqSystem->b[iEq] += - BC->value[IBC]/Grid->DXEC[Grid->nxS-1];//+Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxS-1];
+							//EqSystem->b[iEq] += - BC->value[IBC]/Grid->DXEC[Grid->nxS-1];//+Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxS-1];
+							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxS-1];
 						}
 						break;
 
@@ -275,13 +279,17 @@ void EqSystem_assemble(EqSystem* EqSystem, Grid* Grid, BC* BC, Physics* Physics,
 					case Stencil_Heat:
 
 						if (i==0) { // S
-							EqSystem->b[iEq] += +BC->value[IBC] / Grid->DYEC[0];
+							//EqSystem->b[iEq] += +BC->value[IBC] / Grid->DYEC[0];
+							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[0];
 						} else if (i==1) { // W
-							EqSystem->b[iEq] += +BC->value[IBC] / Grid->DXEC[0];
+							//EqSystem->b[iEq] += +BC->value[IBC] / Grid->DXEC[0];
+							EqSystem->b[iEq] += -Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[0];
 						} else if (i==3) { // E
-							EqSystem->b[iEq] += -BC->value[IBC] / Grid->DXEC[Grid->nxEC-2];
+							//EqSystem->b[iEq] += -BC->value[IBC] / Grid->DXEC[Grid->nxEC-2];
+							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DXEC[Grid->nxEC-2];
 						} else if (i==4) { // N
-							EqSystem->b[iEq] += -BC->value[IBC] / Grid->DYEC[Grid->nyEC-2];
+							//EqSystem->b[iEq] += -BC->value[IBC] / Grid->DYEC[Grid->nyEC-2];
+							EqSystem->b[iEq] += +Vloc[order[i]] * BC->value[IBC] * Grid->DYEC[Grid->nyEC-2];							
 						}
 
 						// For the moment only 0 gradient is implement