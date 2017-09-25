a22ebe6ed8b016ede029f4ea31a4fd42c2911dd0
Arthur Bauville <a.bauville@gmail.com>
2017-09-25 11:27:05 +0900

    support of gcc-7 on linux, also added a ReleaseLinux configuration

--- DebugLinux/src/subdir.mk
+++ DebugLinux/src/subdir.mk
@@ -77,7 +77,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/usr/local/include -O0 -g3 -Wall -c -fmessage-length=0 -fopenmp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc-7 -I/usr/local/include -O0 -g3 -Wall -c -fmessage-length=0 -fopenmp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
