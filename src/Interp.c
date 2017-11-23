43179aa8c0b4bad8716b671f7596f990441252bf
Arthur Bauville <a.bauville@gmail.com>
2017-11-23 17:52:19 +0900

    fixed a bug in the correction for upper convected on particles

--- src/Interp.c
+++ src/Interp.c
@@ -1169,7 +1169,7 @@ void Interp_Stresses_Grid2Particles_Global(Model* Model)
 	compute sigma_xx_0_Grid;
 	compute sigma_xy_0_Grid;
 
-	int Mode = 2; // 0: stress based, 1: strain rate based
+	int Mode = 1; // 0: stress based, 1: strain rate based
 	
 	compute EII;
 	compute* EIICell = (compute*) malloc(Grid->nECTot * sizeof(compute));
@@ -1384,7 +1384,7 @@ void Interp_Stresses_Grid2Particles_Global(Model* Model)
 					//TxyR_def = Txy - Z/G*( -   Txx0*dVydx +   Txx0*dVxdy)
 					
 					sxxPart +=  - Z/G*( - 2.0*Sxx0*ExxPart   - 2.0*Sxy0*dVxdyPart);
-					sxxPart +=  - Z/G*( - 2.0*Sxx0*dVydxPart - 2.0*Sxy0*dVxdyPart);
+					sxxPart +=  - Z/G*( - 1.0*Sxx0*dVydxPart + 1.0*Sxx0*dVxdyPart);
 
 					//sxxPart +=  - Z/G*( - 2*Sxx0*ExxPart   - 2*Sxy0*ExyPart);
 					//sxxPart +=  - Z/G*( - 2*Sxx0*ExyPart - 2*Sxy0*ExyPart);
