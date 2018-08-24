e0903efd0dad7e146ad4f74764f09c10c3417171
Arthur Bauville <a.bauville@gmail.com>
2018-08-24 17:11:32 +0900

    Fix to sticky air viscosity (in the setup file) + Post proc figs

---
+++ PostProcessing/Paper_Decollement/CriticalTaper/CritTaper_Style.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 24 16:27:07 2018
+
+@author: abauville
+"""
+#import numpy as np
+
+class Style():
+    Setting = "Paper"
+#    Setting = "Presentation"
+    if Setting == "Paper":
+        fontdict = {'family': 'Montserrat',
+                    'weight': 'bold',
+                    'size': int(11)
+                    }
+    
+    elif Setting == "Presentation":
+        fontdict = {'family': 'Montserrat',
+                    'weight': 'bold',
+                    'size': int(18)
+                    }
