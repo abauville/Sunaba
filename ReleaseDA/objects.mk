1b90b408d0e3c9af68f9b6bc6ca2b5fc4ca4d1d0
Arthur Bauville <a.bauville@gmail.com>
2019-03-01 11:29:42 +0900

    update Pardiso version in the makefile of ReleaseDA

--- ReleaseDA/objects.mk
+++ ReleaseDA/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lgfortran -lpardiso600-GNU720-X86-64 -lrt -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl
+LIBS := -lgfortran -lpardiso600-GNU80y0-X86-64 -lrt -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl
 
